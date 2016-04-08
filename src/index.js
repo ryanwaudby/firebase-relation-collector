2c3e25d616b3f802ea67d0c2100cb14340363b4c
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2016-04-08 18:44:14 +0100

    Add fetch function with tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-export const sum = (a, b) => {
-  return new Promise(resolve => resolve(a + b));
+import { last, split, keys, map, pick } from 'ramda';
+
+export const fetch = (ref, path) => {
+  return new Promise(resolve => {
+    const parentRef = ref.child(path);
+    const childId = last(split('/', path));
+    const childRef = ref.child(childId);
+
+    parentRef.once('value', snapshot => {
+      const childKeys = keys(snapshot.val());
+
+      Promise.all(childKeys.map(key => collect(childRef.child(key))))
+        .then(resolve);
+    });
+  });
 };
+
+function collect(ref) {
+  return new Promise(resolve => {
+    ref.once('value', snapshot => {
+      resolve(snapshot.val());
+    });
+  });
+}
--- test/index.js
+++ test/index.js
@@ -1,10 +1,36 @@
-import {expect} from 'chai';
-import {sum} from '../';
+import {expect}       from 'chai';
+import {fetch}        from '../src/index.js';
+import FirebaseServer from 'firebase-server';
+import Firebase       from 'firebase';
+
 const {describe, it} = global;
 
-describe('sum', () => {
-  it('should add two numbers correctly', async () => {
-    const result = await sum(10, 20);
-    expect(result).to.be.equal(30);
+const startServer = (hostname, data) => new FirebaseServer(5000, hostname, data);
+
+describe('fetch', () => {
+  it('collects childs correctly', async () => {
+    const host = 'localhost.firebaseio.com';
+    const data = {
+      "accounts": {
+        "myaccount": {
+          "items": {
+            "child1": true,
+            "child2": true
+          }
+        }
+      },
+      "items": {
+        "child1": { "id": "child1", "some": "prop" },
+        "child2": { "id": "child2" },
+        "child3": { "id": "child3" }
+        }
+    };
+
+    startServer('ws://localhost.firebaseio.com', data);
+
+    const ref = new Firebase('ws://localhost.firebaseio.com:5000');
+    const children = await fetch(ref, 'accounts/myaccount/items');
+
+    expect(children).to.deep.equal([{ id: 'child1', some: 'prop' }, { id: 'child2' }]);
   });
 });
