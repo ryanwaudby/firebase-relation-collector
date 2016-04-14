75c86ee3bf6882698846f0db98f79c8f81f9c875
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2016-04-14 14:25:43 +0100

    Update test structure to use localhost

--- test/index.js
+++ test/index.js
@@ -1,70 +1,81 @@
-import { expect }     from 'chai';
-import { collect }    from '../src/index.js';
-import FirebaseServer from 'firebase-server';
-import Firebase       from 'firebase';
+import { expect }         from 'chai';
+import { collect }        from '../src/index.js';
+import FirebaseServer     from 'firebase-server';
+import proxyquire         from 'proxyquire';
+import originalWebsocket  from 'faye-websocket';
 
 const {describe, it} = global;
-
-const host = 'ws://localhost.firebaseio.com';
-const startServer = (hostname, data) =>
-  new FirebaseServer(5000, hostname, data);
-
-let server;
-
-before(() => {
-  const data = {
-    accounts: {
-      myaccount: {
-        items: {
-          child1: true,
-          child2: true
-        }
-      }
-    },
-    items: {
-      child1: { id: 'child1', some: 'prop' },
-      child2: { id: 'child2' },
-      child3: { id: 'child3' }
-    },
-    members: {
-      mymembership: {
-        friends: {
-          member1: true,
-          member3: true
-        }
-      },
-      member1: { some: 'member' },
-      member2: { not: 'a friend' },
-      member3: { my: 'other friend' }
-    }
-  };
-  server = startServer(host, data);
-});
-
-after(() => {
-  server.close();
-});
+const host = 'dummy.firebaseio.test';
+const port = 5000;
+const startServer = data => new FirebaseServer(port, `localhost:${port}`, data);
 
 describe('collect', () => {
   it('collects relations with default key', async () => {
-    const ref = new Firebase(`${host}:5000`);
+    const data = {
+      accounts: {
+        myaccount: {
+          items: {
+            child1: true,
+            child2: true
+          }
+        }
+      },
+      items: {
+        child1: { id: 'child1', some: 'prop' },
+        child2: { id: 'child2' },
+        child3: { id: 'child3' }
+      }
+    };
+
+    const ref = new Firebase(`${host}:${port}`);
     const path = 'accounts/myaccount/items';
+    const server = startServer(data);
     const relations = await collect(ref, path);
 
-    expect(relations).to.deep.equal([
-      { id: 'child1', some: 'prop' },
-      { id: 'child2' }
-    ]);
+    server.close(() => {
+      expect(relations).to.deep.equal([
+        { id: 'child1', some: 'prop' },
+        { id: 'child2' }
+      ]);
+    });
   });
 
   it('collects relations with specifc key', async () => {
-    const ref = new Firebase(`${host}:5000`);
+    const data = {
+      members: {
+        mymembership: {
+          friends: {
+            member1: true,
+            member3: true
+          }
+        },
+        member1: { some: 'member' },
+        member2: { not: 'a friend' },
+        member3: { my: 'other friend' }
+      }
+    };
+
+    const ref = new Firebase(`${host}:${port}`);
     const path = 'members/mymembership/friends';
+    const server = startServer(data);
     const relations = await collect(ref, path, 'members');
 
-    expect(relations).to.deep.equal([
-      { some: 'member' },
-      { my: 'other friend' }
-    ]);
+    server.close(() => {
+      expect(relations).to.deep.equal([
+        { some: 'member' },
+        { my: 'other friend' }
+      ]);
+    });
   });
+});
+
+const Firebase = proxyquire('firebase', {
+  'faye-websocket': {
+    Client(url) {
+      const replaced = url
+        .replace(host, 'localhost')
+        .replace('wss://', 'ws://');
+      return new originalWebsocket.Client(replaced);
+    }
+  }
 });
